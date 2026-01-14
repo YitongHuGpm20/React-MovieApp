b8627465962c334a3385329636850932874b772b
YitongHuGpm20 <yitongh@ourenglishinc.com>
2026-01-14 15:08:16 -0800

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// Hooks
+import { useState, useEffect } from 'react'
+
+// Assets
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+      
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">Movies</ span> You'll Enjoy Without The Hassle</h1>
+        </header>
+      </div>
+    </main>
+  )
+}
+
+export default App
