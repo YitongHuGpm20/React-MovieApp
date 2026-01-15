f5c921ce64eb7c8dd9e4322b42440a24f4726e08
YitongHuGpm20 <43768506+YitongHuGpm20@users.noreply.github.com>
2026-01-14 19:14:18 -0800

    Created Movie Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import './App.css'
 // Components
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 // API
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -79,7 +80,7 @@ const App = () => {
           ) : (
             <ul>
               {movies.map((movie) => (
-                <p className="text-white">{movie.title}</p>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
